ea9496b6a87a40079820e67b077126128abaf330
Jan Zięba <yasiekz@gmail.com>
2021-04-11 09:29:35 +0200

    add phone number to patient request

--- src/main/java/io/yasiekz/github/hexagonaldddexample/application/dto/patient/CreatePatientRequest.java
+++ src/main/java/io/yasiekz/github/hexagonaldddexample/application/dto/patient/CreatePatientRequest.java
@@ -1,6 +1,7 @@
 package io.yasiekz.github.hexagonaldddexample.application.dto.patient;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Value;
 
@@ -13,5 +14,7 @@ public class CreatePatientRequest {
     @NotEmpty
     @Size(max = 70)
     String idNumber;
-
+    @NotEmpty
+    @Pattern(regexp = "^\\+48[0-9]{9}")
+    String phoneNumber;
 }
